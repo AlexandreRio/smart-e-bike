45a318a75c923996863b4118e260c49a5c5ac3e9
Ellyn <ellyn85000@hotmail.fr>
2015-02-03 17:38:55 +0100

    Commentaire

--- Bike-app/app/src/main/java/al/esir/bike_app/MainActivity.java
+++ Bike-app/app/src/main/java/al/esir/bike_app/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+// main
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
