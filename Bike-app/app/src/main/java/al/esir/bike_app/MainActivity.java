0e4ae25a396cf16c92b736da818e021fe6327237
Ellyn <ellyn85000@hotmail.fr>
2015-02-10 17:13:19 +0100

    Lien entre boutton Favoris et Interface Favoris

--- Bike-app/app/src/main/java/al/esir/bike_app/MainActivity.java
+++ Bike-app/app/src/main/java/al/esir/bike_app/MainActivity.java
@@ -1,9 +1,11 @@
 package al.esir.bike_app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +36,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getFavorisInterface(View view){
+        Intent intent = new Intent(this, Favoris.class);
+        startActivity(intent);
     }
 }
